092109cab1edcd76e5b70f45f5d326dd620a6adf
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-06-11 15:10:06 +0100

    Update de_analysis.R
    Two key updates:
    * Filtering out of unwanted samples based on metadata values
    * Adding a line to confirm when common dispersion has been calculated

--- de_analysis.R
+++ de_analysis.R
@@ -6,53 +6,55 @@ library(edgeR)
 
 dat <- read.delim("PvR_isoformCounts_all.txt", header = TRUE)
 
-# check dimensions
-
-dim(dat)                      
-
-# rearrange columns so that isoform identifier, gene name and gene type in the first 3 columns, followed by the counts across 176 samples (for 88 patients)
+# rearrange columns
 
 dat <- dat[,c(1,10,11,2:9,12:ncol(dat))]
-head(dat)
 
-# remove rows that with counts summing to zero (i.e. no expression for transcript across all samples)
+# remove rows that add up to zero
 
 dat <- dat[rowSums(dat[,4:ncol(dat)]) != 0,]
 
-# create DGEList using edgeR package
+# create DGEList data class
 
-y <- DGEList(counts=dat[,4:179], genes=dat[,1:3])
+y <- DGEList(counts=dat[,4:ncol(dat)], genes=dat[,1:3])
+
+# remove columns corresponding to samples that should be excluded from the analyses
+
+# import list of patients to remove
+
+patients.remove <- read.delim("patients_remove.txt", header = F)
+
+# convert list to vector
+
+patients.remove <- as.vector(t(patients.remove))
+
+# filter DGEList using the samples object
+
+keep <- !sub("_.*","",rownames(y$samples)) %in% patients.remove
+y <- y[,keep]
 
 # TMM normalisation
 
 y <- calcNormFactors(y)
-y$samples
 
 # create design matrix 
 
-# vector of all sample names in format patientid_tumourtype
 samples <- rownames(y$samples)
-
-# extract patient IDs and format as factors
-
 patient <- sub("_.*","",samples)
 patient <- factor(patient)
-
-# extract tumour types (i.e. primary or recurrent) and format as factors
-
 tumour.type <- sub(".*_","",samples)
 tumour.type <- ifelse(tumour.type == "Primary","P", ifelse(tumour.type == "Recurrent", "R", tumour.type))
 tumour.type <- as.factor(tumour.type)
-
-# create design matrix
-
 design <- model.matrix(~ patient + tumour.type)
 rownames(design) <- colnames(y)
-design
 
-# estimate dispersions
+# estimate dispersion
 
 y <- estimateGLMCommonDisp(y, design)
+
+cd <- y$common.dispersion
+write(cd, "commondispersion.txt")
+
 y <- estimateGLMTrendedDisp(y, design)
 y <- estimateGLMTagwiseDisp(y, design)
 
@@ -60,9 +62,6 @@ y <- estimateGLMTagwiseDisp(y, design)
 
 fit <- glmFit(y, design)
 lrt <- glmLRT(fit)
-
-# create dataframe of results and write to csv
-
 res <- topTags(lrt)
 write.table(res, "results.csv")
 
@@ -76,5 +75,3 @@ summary(de <- decideTestsDGE(lrt))
 detags <- rownames(y)[as.logical(de)]
 plotSmear(lrt, de.tags=detags)
 abline(h=c(-1, 1), col="blue")
-
-
