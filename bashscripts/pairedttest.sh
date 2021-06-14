d94bd100f093316b19d70f7ab2d095f9aded1626
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-06-14 15:42:04 +0100

    Update pairedttest.sh

--- bashscripts/pairedttest.sh
+++ bashscripts/pairedttest.sh
@@ -5,16 +5,16 @@
 #$ -l h_rt=24:00:00
 
 # Ask for some memory (by default, 1G, without a request)
-#$ -l h_vmem=2G
+#$ -l h_vmem=6G
 
 # Ask for output and error files to be sent to a specific folder
-#$ -o /nobackup/bs20chlb/outdir/job-$JOB_ID.stdout
-#$ -e /nobackup/bs20chlb/outdir/job-$JOB_ID.stderr
+#$ -o /nobackup/bs20chlb/outdir/pairedttest/job-$JOB_ID.stdout
+#$ -e /nobackup/bs20chlb/outdir/pairedttest/job-$JOB_ID.stderr
 
 # Send emails when job starts and ends
 #$ -m be
 
 # Now run the job
 module load R
-R CMD BATCH /nobackup/bs20chlb/scripts/isoformproject/dea/pairedttest.R /nobackup/bs20chlb/outdir/R-${JOB_ID}.Rout
+R CMD BATCH /nobackup/bs20chlb/scripts/isoformproject/dea/pairedttest.R /nobackup/bs20chlb/outdir/pairedttest/R-${JOB_ID}.Rout
 
