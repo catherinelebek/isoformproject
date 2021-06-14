f31c73a7c2330a593d74fa90acfa1bd64e71651e
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-06-14 12:55:52 +0100

    Update edgeR.sh

--- bashscripts/edgeR.sh
+++ bashscripts/edgeR.sh
@@ -5,12 +5,16 @@
 #$ -l h_rt=48:00:00
 
 # Ask for some memory (by default, 1G, without a request)
-#$ -l h_vmem=6G
+#$ -l h_vmem=12G
+
+# Ask for output and error files to be sent to a specific folder
+#$ -o /nobackup/bs20chlb/outdir/job-$JOB_ID.stdout
+#$ -e /nobackup/bs20chlb/outdir/job-$JOB_ID.stderr
 
 # Send emails when job starts and ends
 #$ -m be
 
 # Now run the job
 module load R
-R CMD BATCH de_analysis.R
+R CMD BATCH /nobackup/bs20chlb/scripts/isoformproject/dea/edgeR.R /nobackup/bs20chlb/outdir/R-${JOB_ID}.Rout
 
