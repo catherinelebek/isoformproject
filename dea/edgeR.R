86162ec971d95db64e05729611b474af6491c458
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-06-14 15:01:00 +0100

    Update edgeR.R

--- dea/edgeR.R
+++ dea/edgeR.R
@@ -16,7 +16,13 @@ dat <- read.delim("PvR_isoformCounts_all.txt", header = TRUE)
 
 dat <- dat[,c(1,10,11,2:9,12:ncol(dat))]
 
+# check columns in correct order
+
+colnames(dat)
+
 # create DGEList data class
+# columns 4 to final column are counts per sample
+# columns 1 to 3 are transcript ID, gene name and gene type
 
 y <- DGEList(counts=dat[,4:ncol(dat)], genes=dat[,1:3])
 
@@ -32,13 +38,13 @@ patients.remove <- as.vector(t(patients.remove))
 
 # filter DGEList using the samples object
 
-keep <- !sub("_.*","",rownames(y$samples)) %in% patients.remove
-y <- y[,keep]
+keep <- !sub("_.*","",rownames(y$samples)) %in% patients.remove # indexes for patient samples not in the patient remove file
+y <- y[,keep] # subsetting y
 
 # remove rows that add up to zero
 
-keep <- rowSums(y$count) != 0
-y <- y[keep,]
+keep <- rowSums(y$count) != 0 # indexes for transcripts with non-zero reads for at least one sample
+y <- y[keep,] # subsetting y
 
 # TMM normalisation
 
@@ -55,22 +61,40 @@ tumour.type <- as.factor(tumour.type)
 design <- model.matrix(~ patient + tumour.type)
 rownames(design) <- colnames(y)
 
-# estimate dispersion
+# estimate common dispersion
 
 y <- estimateGLMCommonDisp(y, design)
 
-cd <- y$common.dispersion
+# print out dispersion estimate to validate this stage in code was reached
 
+cd <- y$common.dispersion
 setwd(outputdata)
 write(cd, "commondispersion.txt")
 
+# estimate trended dispersion
+
 y <- estimateGLMTrendedDisp(y, design)
+
+# estimate tagwise dispersion
+
 y <- estimateGLMTagwiseDisp(y, design)
+
+# plot dispersions
+
+plotBCV(y)
 
 # do differential transcript expression
 
+# fitting transcript-wise general linear models
+
 fit <- glmFit(y, design)
+
+# conduct likelihood ratio test for last coefficient in linear model (by default)
+
 lrt <- glmLRT(fit)
+
+# print results of top 10 most differentially expressed transcripts 
+
 res <- topTags(lrt)
 write.table(res, "edgeRresults.csv")
 
