1e545938740ab11355b2e90952b5f3060c1017e7
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-06-23 09:49:39 +0100

    Create listtranscripts.sh

---
+++ bashscripts/listtranscripts.sh
@@ -0,0 +1,19 @@
+# Run from the current directory and with current environment
+#$ -cwd -V
+
+# Ask for some time (hh:mm:ss max of 48:00:00)
+#$ -l h_rt=12:00:00
+
+# Ask for some memory (by default, 1G, without a request)
+#$ -l h_vmem=6G
+
+# Ask for output and error files to be sent to a specific folder
+#$ -o /nobackup/bs20chlb/outdir/resultsanalysis/job-$JOB_ID.stdout
+#$ -e /nobackup/bs20chlb/outdir/resultsanalysis/job-$JOB_ID.stderr
+
+# Send emails when job starts and ends
+#$ -m be
+
+# Now run the job
+module load R
+R CMD BATCH  /nobackup/bs20chlb/scripts/isoformproject/resultsanalysis/listranscripts.R /nobackup/bs20chlb/outdir/resultsanalysis/R-${JOB_ID}.Rout
