26a165a6b9e94d2b9d940c300c6dd370b9b17c2a
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-06-23 09:44:52 +0100

    Update listtranscripts.R

--- resultsanalysis/listtranscripts.R
+++ resultsanalysis/listtranscripts.R
@@ -1,20 +1,20 @@
-check <- read.delim("~/Documents/Semester3/Project/Results/localresults/filter3/PvR_isoformCounts_filtered.txt",header = T, sep = " ")
+fullcounts <- read.delim("/nobackup/bs20chlb/inputdata/archive/PvR_isoformCounts_all.txt",header = T, sep = " \t")
 
-transcripts <- vector(mode = "list", length = nrow(check))
+transcripts <- vector(mode = "list", length = nrow(fullcounts))
 
 # create an empty dataframe for every transcript
 
 for (i in 1:length(transcripts)){
   temp <- data.frame(matrix(ncol = 2, nrow = 43))
   colnames(temp) <- c("Primary","Recurrent")
-  rownames(temp) <- unique(sub("_.*","",colnames(check[,4:ncol(check)])))
+  rownames(temp) <- unique(sub("_.*","",colnames(fullcounts[,4:ncol(fullcounts)])))
   transcripts[[i]] <- temp
 }
 
 
 for (i in 1:length(transcripts)){
   for (j in 1:nrow(transcripts[[1]])){
-  transcripts[[i]][j,1] <- check[check$EnsID == names(transcripts)[i], colnames(check) == paste(rownames(transcripts[[i]][j,]),"_P",sep="")]
-  transcripts[[i]][j,2] <- check[check$EnsID == names(transcripts)[i], colnames(check) == paste(rownames(transcripts[[i]][j,]),"_R",sep="")]
+  transcripts[[i]][j,1] <- fullcounts[fullcounts$EnsID == names(transcripts)[i], colnames(fullcounts) == paste(rownames(transcripts[[i]][j,]),"_P",sep="")]
+  transcripts[[i]][j,2] <- fullcounts[fullcounts$EnsID == names(transcripts)[i], colnames(fullcounts) == paste(rownames(transcripts[[i]][j,]),"_R",sep="")]
   }
 }
