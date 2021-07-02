b0def2c1717df93964e4cc26784941291bf69bb5
catherinelebek <74797621+catherinelebek@users.noreply.github.com>
2021-07-02 15:20:30 +0100

    update

--- dataexploration/isoforms/seconddata/pca.R
+++ dataexploration/isoforms/seconddata/pca.R
@@ -1,16 +1,18 @@
 library(stringr)
 
 counts.full <- read.delim("~/Documents/Semester3/Project/Results/filtered_data/isoforms/seconddata/PvR_isoformCounts_filtered.txt",header = T, sep = "\t")
-head(counts)
+head(counts.full)
 
 counts <- counts.full
 
 rownames(counts) <- counts[,1]
 counts <- counts[,c(-1,-2,-3)]
 
-idx_primary <- colnames(counts)
+func <- function(x){x + 0.01}
 
-colnames(counts)
+counts <- apply(counts, 1:2, func)
+
+head(counts)
 
 cols <- gsub(".{2}$","",colnames(counts))
 cols <- unique(cols)
@@ -27,41 +29,32 @@ table(gsub("_P$","",colnames(counts.primary)) == gsub("_R$","",colnames(counts.r
 head(counts.primary)
 head(counts.recurrent)
 
-counts.primary <- is.
-
-
-counts.primary.log <- apply(counts.primary, c(1,2), log2)
-counts.recurrent.log <- apply(counts.recurrent, c(1,2), log2)
-counts.delta.log <- counts.recurrent.log - counts.primary.log
-
-counts.primary[1:5,1]
-counts.recurrent[1:5,1]
-counts.primary.log[1:5,1]
-counts.recurrent.log[1:5,1]
-counts.delta.log[1:5,1]
-
-
+counts.primary <- apply(counts.primary, c(1,2), log2)
+counts.recurrent <- apply(counts.recurrent, c(1,2), log2)
 counts.delta <- counts.recurrent - counts.primary
 
-counts.delta.log <- counts.delta.log[is.infinite(counts.delta.log)] <- -99999
+head(counts.delta)
 
-head(counts.delta.log)
+
+dim(counts.delta)
+
+table(rowSums(abs(counts.delta)) == Inf)
+table(is.na(counts.delta))
 
 # need patients to be the rows
-
 
 counts.delta <- na.omit(counts.delta)
 idx <- rowSums(abs(counts.delta)) != Inf
 counts.delta <- counts.delta[idx,]
 
 
-pca.res <- prcomp(counts.delta)
+pca.res <- prcomp(counts.delta, scale. = T)
 pca.res.rot <- pca.res$rotation
 pca.res.rot <- as.data.frame(pca.res.rot)
 rownames(pca.res.rot) <- gsub(".{2}$","",rownames(pca.res.rot))
 
 metadata <- read.csv("/Users/catherinehogg/Documents/Semester3/Project/InputData/isoforms/seconddata/MetaData_GT_250621.txt",
-                                 header = TRUE, "\t")
+                     header = TRUE, "\t")
 
 head(metadata)
 
@@ -69,6 +62,8 @@ pca.res.rot <- merge(pca.res.rot, metadata[,c("Patient.ID","NES")], by.x = "row.
 pca.res.rot$type <- ifelse(pca.res.rot$NES > 0, "Up", "Down")
 pca.res.rot$type <- as.factor(pca.res.rot$type)
 
-plot(pca.res$rotation[,1],pca.res$rotation[,2],xlab="loading 1",ylab="loading 2", 
-     main="Loadings", col = pca.res.rot$type, pch = 16)
+plot(pca.res.rot$PC1,pca.res.rot$PC2,xlab="loading 1",ylab="loading 2", 
+     main="Loadings - LFC isoform expression", col = pca.res.rot$type, pch = 16)
+legend(0.1,0.3,c("Down","Up"), col = c(1,2), pch = 16)
 
+pca.res.rot[order(pca.res.rot$PC2),]
